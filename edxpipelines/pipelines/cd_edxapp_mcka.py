8156a29c2c6004db5fb784c942f486cec7a7597f
Brian Beggs <bbeggs@edx.org>
2017-07-13 10:20:03 -0400

    fix pathing

--- edxpipelines/pipelines/cd_edxapp_mcka.py
+++ edxpipelines/pipelines/cd_edxapp_mcka.py
@@ -1,9 +1,15 @@
 """
 Deployment pipeline for McKinsey Academy.
 """
-from edxpipelines.patterns.apros import \
-    generate_deployment_service_pipelines
+import sys
+from os import path
+
+sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+
+from edxpipelines.patterns.apros import generate_deployment_service_pipelines
 from edxpipelines.pipelines.script import pipeline_script
+
+
 
 
 def install_pipelines(configurator, config):
