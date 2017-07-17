20622f249d1776cb759a8263c9c5c788134a3a59
Jawad <ranajawadtahir@gmail.com>
2017-07-17 14:06:55 +0500

    -fixed failing tests

--- edxpipelines/pipelines/cd_edxapp_mcka.py
+++ edxpipelines/pipelines/cd_edxapp_mcka.py
@@ -4,12 +4,11 @@ Deployment pipeline for McKinsey Academy.
 import sys
 from os import path
 
+# pylint: disable=wrong-import-position
 sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
 
 from edxpipelines.pipelines.script import pipeline_script
 from edxpipelines.patterns.apros import generate_deployment_service_pipelines
-
-
 
 
 def install_pipelines(configurator, config):
