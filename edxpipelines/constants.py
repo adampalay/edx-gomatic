10115eb6b2f18adb1c9809743e95117e6dba356f
Brian Beggs <bbeggs@edx.org>
2017-07-19 22:21:20 -0400

    Fix db username

--- edxpipelines/constants.py
+++ edxpipelines/constants.py
@@ -135,7 +135,6 @@ PUBLIC_PRIVATE_PUSH_FILENAME = 'pub_priv_push.yml'
 # identify a build.
 DEPLOYMENT_PIPELINE_LABEL_TPL = '${{{.material_name}[:7]}}-${{COUNT}}'.format
 DB_MIGRATION_USER = 'migrate001'
-DB_MIGRATION_USER = 'migrate'
 # Keep this for old edxapp pipelines, remove when cd_edxapp.py is gone
 MIGRATION_OUTPUT_DIR_NAME = 'migrations'
 # new Migration output dir name:
